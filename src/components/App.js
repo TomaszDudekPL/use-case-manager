be233249891c5a2f7c47aaa157cfa65cc6b267e4
Tomasz Dudek <tdkontakt@gmail.com>
2017-05-10 21:42:00 +0200

    Created login screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import {FormGroup, FormControl, InputGroup, Glyphicon, ControlLabel, Checkbox, Button} from 'react-bootstrap'
+import '../assets/stylesheets/base.scss'
 
-import '../assets/stylesheets/base.scss';
 
+class App extends React.Component {
 
-const App = ({ name }) => {
-  return (
-    <h1>Hello, {name}!</h1>
-  );
-};
+    _check() {
+        let content = null;
+        let username = document.getElementById('username');
+        let password = document.getElementById('password');
+        if(username.value && password.value){
+            content = "content pełne!"
+        } else{
+            content = 'content puste';
+        }
+        console.log(content);
+        return content;
+    }
 
-App.propTypes = {
-  name: PropTypes.string,
-};
+    render() {
+        return <section className="section__log">
+            <div className="div__log">
+                <form>
+                    <FormGroup>
+                        <ControlLabel className="pdn10">Login</ControlLabel>
+                        <span>{this._check}</span>
+                        <InputGroup className="pdn10">
+                            <InputGroup.Addon>@</InputGroup.Addon>
+                            <FormControl type="text" placeholder="Username" id="username"/>
+                        </InputGroup>
+                        <InputGroup className="pdn10">
+                            <InputGroup.Addon><Glyphicon glyph="star"/></InputGroup.Addon>
+                            <FormControl type="password" placeholder="Password" id="password"/>
+                        </InputGroup>
+                        <div className="div__ctrl">
+                            <div className="pdn10-left"><Checkbox readOnly>Keep me logged in</Checkbox></div>
+                            <div className="pdn10-right"><Button onClick={()=> this._check() } bsStyle="info" id="btn__login" type="button">Submit</Button></div>
+                        </div>
+                    </FormGroup>
+                </form>
+            </div>
+        </section>
 
-export default App;
+    }
+
+}
+
+export default App
