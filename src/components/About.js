187979e62088d9abefcd7b9bf97fb0267abd94ed
Tomasz Dudek <tdkontakt@gmail.com>
2017-05-11 12:18:52 +0200

    File About added with new component.

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class About extends React.Component {
+
+    render() {
+        return (
+            <div className="about">
+                <p>Super Duper Example App!</p>
+            </div>
+        );
+    }
+}
+export default About
