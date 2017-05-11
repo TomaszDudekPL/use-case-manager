6fd6920e6523159f28f6a072389eded415bea18c
Tomasz Dudek <tdkontakt@gmail.com>
2017-05-11 12:17:00 +0200

    1.removed error signal at start in login panel, 2. color-signal is now only on input e-mail not whole panel, 3. Building routing.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FormGroup, FormControl, InputGroup, Glyphicon, ControlLabel, Checkbox, Button} from 'react-bootstrap'
+import {FormGroup, FormControl, InputGroup, Glyphicon, ControlLabel, Checkbox, Button, Form} from 'react-bootstrap'
 import '../assets/stylesheets/base.scss'
 
 const initialState = {
@@ -15,6 +15,7 @@ class App extends React.Component {
             password: ''
         }
     }
+
     getValidationState() {
         const usernameLength = this.state.username.length;
         const username = this.state.username;
@@ -22,7 +23,7 @@ class App extends React.Component {
         let result = oRegExp.test(username);
         if (usernameLength >= 9 && result) return 'success';
         else if (usernameLength >= 3 || typeof username === 'number') return 'warning';
-        else if (!username || usernameLength < 3) return 'error';
+
     }
 
     globalValidation() {
@@ -30,38 +31,38 @@ class App extends React.Component {
         const password = document.getElementById('password').value;
         let warning = document.getElementById('warning');
         if (!(username === initialState.username && password === initialState.password)) {
-            warning.innerHTML = 'Username or Password incorrect'
-        } else {
-            warning.innerHTML = ''
+            warning.innerHTML = 'Username or Password incorrect' }
+        else {
+                warning.innerHTML = '';
+
+            }
         }
-    }
+
 
     render() {
         return <section className="section__log">
             <div className="div__log">
-                <form>
+                <Form>
                     <FormGroup validationState={this.getValidationState()}>
                         <ControlLabel className="pdn10">Login</ControlLabel>
-                        <div id="warning" className="pdn10-left"></div>
+                        <div id="warning" className="warning pdn10-left"></div>
                         <InputGroup className="pdn10">
                             <InputGroup.Addon>@</InputGroup.Addon>
                             <FormControl type="text" placeholder="Username" id='username' value={this.state.value}
                                          onChange={ e => this.setState({username: e.target.value})}/>
                             <FormControl.Feedback />
                         </InputGroup>
-                        <InputGroup className="pdn10">
+                    </FormGroup>
+                    <InputGroup className="pdn10">
                             <InputGroup.Addon><Glyphicon glyph="star"/></InputGroup.Addon>
                             <FormControl type="password" placeholder="Password" id='password' value={this.state.value}
                                          onChange={ e => this.setState({password: e.target.value})}/>
                         </InputGroup>
                         <div className="div__ctrl">
                             <div className="pdn10-left"><Checkbox readOnly>Keep me logged in</Checkbox></div>
-                            <div className="pdn10-right"><Button bsStyle="info" id="btn__login"
-                                                                 type="button"
-                                                                 onClick={this.globalValidation}>Submit</Button></div>
+                            <div className="pdn10-right"><Button bsStyle="info" id="btn__login" type="button" onClick={this.globalValidation}>Submit</Button></div>
                         </div>
-                    </FormGroup>
-                </form>
+                </Form>
             </div>
         </section>
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App';
+import About from './components/About'
 
 
-
-render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}/>
+        <Route path="/about" component={About}/>
+    </Router>
+        ), document.getElementById('root'));
