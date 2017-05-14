8014d65799451480f1abd52c30b427871f42c41c
Tomasz Dudek <tdkontakt@gmail.com>
2017-05-14 20:55:53 +0200

    Added navigation bar. Added routing and user authentication process.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import { Router, Route, browserHistory } from 'react-router'
-import App from './components/App';
-import About from './components/About'
+import ReactDOM from 'react-dom';
+import { Nav, Navbar, NavItem} from 'react-bootstrap'
+import {AuthButton, PrivateRoute, Public, Protected, Login} from './components/App'
+import {BrowserRouter as NewRouter, Route, Link} from 'react-router-dom'
+import './assets/stylesheets/base.scss';
+
+const AuthExample = () => (
+    <NewRouter>
+        <div>
+            <Navbar inverse collapseOnSelect fixedTop>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">Use Case Manager</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem><Link to="/public" className="clr-white">Aplication</Link></NavItem>
+                    </Nav>
+                    <Nav pullRight><AuthButton />
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <Route path="/public" component={Public}/>
+            <Route path="/login" component={Login}/>
+            <PrivateRoute path="/protected" component={Protected}/>
+        </div>
+    </NewRouter>
+);
 
 
-ReactDOM.render((
-    <Router history={browserHistory}>
-        <Route path="/" component={App}/>
-        <Route path="/about" component={About}/>
-    </Router>
-        ), document.getElementById('root'));
+ReactDOM.render(
+    <AuthExample />,
+    document.getElementById('root')
+);
